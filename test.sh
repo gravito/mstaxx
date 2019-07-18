825c6f34d24ae1581c2021d616b0fb7eb49e675d
gravito <aayush.shrut@gmail.com>
2019-07-18 10:25:40 +0530

    v2

--- test.sh
+++ test.sh
@@ -7,11 +7,17 @@ sleep 10
 var2=$(top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1)
 echo $var2
 var3=50
+for i in $var1
+do
+sleep 30
 if (( $(echo "$var2 > $var3" | bc -l) )); then
     kubectl scale deployment frontend --replicas=5
 else
     echo "CPU Load not spiking"
 fi
+done
 kubectl get pods
 sleep 10
 killall yes
+kubectl scale deployment frontend --replicas=3
+
